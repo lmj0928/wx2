cad8bb5e996afc54464536303d8b45460cb058f1
lmj <2418769974@qq.com>
2019-06-10 09:01:12 +0800

    微信小程序日考2

---
+++ test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <span>微信小程序日考2</span>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
